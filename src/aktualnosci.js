3c1b04b016b4066f361fd5320638b40f938e2a55
szymeck <szymek.sobierajski@gmail.com>
2023-09-20 15:20:29 +0200

    zmiany2

--- src/aktualnosci.js
+++ src/aktualnosci.js
@@ -17,6 +17,22 @@ const Aktualnosci = () => {
       <Bar title={"Aktualności"} />
       <div className="aktualnosci">
         <div className="aktualnosci-container">
+        <Link to="/rekrutacja"
+              onClick={() => {
+                handleClick();
+              }} ><div className="aktualnosci-item">
+            <div className="item-img">
+              <img src="./aktualnosci-2.jpg" alt="img"></img>
+            </div>
+            <div className="item-text">
+              <h3>
+              Od Stycznia 2024, nowa grupa dla dzieci w wieku 2,5-3 lat
+              </h3>
+              <p>Dowiedz się wiecej</p>
+            </div>
+          </div>
+          </Link >
+          
           <Link to="/rekrutacja"
               onClick={() => {
                 handleClick();
@@ -27,21 +43,6 @@ const Aktualnosci = () => {
             <div className="item-text">
               <h3>
               RUSZYŁA REKRUTACJA DO NASZEGO PRZEDSZKOLA !!!
-              </h3>
-              <p>Dowiedz się wiecej</p>
-            </div>
-          </div>
-          </Link >
-          <Link to="/rekrutacja"
-              onClick={() => {
-                handleClick();
-              }} ><div className="aktualnosci-item">
-            <div className="item-img">
-              <img src="./aktualnosci-2.jpg" alt="img"></img>
-            </div>
-            <div className="item-text">
-              <h3>
-              Od Stycznia 2024, nowa grupa dla dzieci w wieku 2,5-3 lat
               </h3>
               <p>Dowiedz się wiecej</p>
             </div>
