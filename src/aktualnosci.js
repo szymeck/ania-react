b05904b491d21cd5f0bae3de4afc05a9ecadc886
Szymon Sobierajski <93445715+szymeck@users.noreply.github.com>
2025-04-09 13:34:02 +0200

    Update aktualnosci.js

--- src/aktualnosci.js
+++ src/aktualnosci.js
@@ -17,21 +17,7 @@ const Aktualnosci = () => {
       <Bar title={"Aktualności"} />
       <div className="aktualnosci">
         <div className="aktualnosci-container">
-        <Link to="/rekrutacja"
-              onClick={() => {
-                handleClick();
-              }} ><div className="aktualnosci-item">
-            <div className="item-img">
-              <img src="./aktualnosci-2.jpg" alt="img"></img>
-            </div>
-            <div className="item-text">
-              <h3>
-              Od Stycznia 2024, nowa grupa dla dzieci w wieku 2,5-3 lat
-              </h3>
-              <p>Dowiedz się wiecej</p>
-            </div>
-          </div>
-          </Link >
+        
           
           <Link to="/rekrutacja"
               onClick={() => {
